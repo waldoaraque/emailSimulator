5b8292a10c083a180e4fbb406a7e4f1f9b0342ed
waldev <waldoaraque10@gmail.com>
2020-04-01 17:55:48 +0200

    adding functions to validate some data from the DOM

--- js/app.js
+++ js/app.js
@@ -5,15 +5,58 @@ const message = document.getElementById('mensaje');
 const sendBtn = document.getElementById('enviar');
 
 //EVENT LISTENERS
+eventListeners();
 
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', getApp);
-}
 
+    email.addEventListener('blur', validate);
+    issue.addEventListener('blur', validate);
+    message.addEventListener('blur', validate);
+}
 
 
 
 //FUNCTIONS
 function getApp() {
     sendBtn.disabled = true;
+}
+
+function validate() {
+
+    validateLong(this);
+
+    if (this.type === 'email') {
+        validateEmail(this);
+    }
+
+    let errors = document.querySelectorAll('.error');
+
+    if (email.value !== '' && issue.value !== '' && message.value !== '') {
+        if (errors.length === 0) {
+            sendBtn.disabled = false;
+        }
+    } 
+}
+
+function validateLong(data) {
+    if (data.value.length > 0) {
+        data.style.borderBottomColor = 'green';
+        data.classList.remove('error');
+    } else {
+        data.style.borderBottomColor = 'red';
+        data.classList.add('error');
+    }
+}
+
+function validateEmail(data) {
+    let m = data.value;
+    
+    if (m.indexOf('@') !== -1) {
+        data.style.borderBottomColor = 'green';
+        data.classList.remove('error');
+    } else {
+        data.style.borderBottomColor = 'red';
+        data.classList.add('error');
+    }
 }
